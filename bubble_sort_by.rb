bd14d9a24dd13e68b97cdbe530f96c167accac4e
Pacifique Linjanja <pacilinja1@icloud.com>
2020-12-12 22:23:57 +0100

    Put example block for testing

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,16 +1,19 @@
-def bubble_sort_by (arr) 
-    puts arr if arr.length <= 1
-    n = arr.length - 1
-  
-    loop do
-      swapped = false
-      n.times do |i|
-        if yield(arr[i], arr[i + 1]).positive?
-          arr[i], arr[i + 1] = arr[i + 1], arr[i]
-          swapped = true
-        end
+def bubble_sort_by(arr)
+  puts arr if arr.length <= 1
+  n = arr.length - 1
+  loop do
+    swapped = false
+    n.times do |i|
+      if yield(arr[i], arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swap = true
       end
-      break if swapped == false
     end
-    p arr
+    break if swapped == false
+  end
+  p arr
+end
+
+bubble_sort_by(['hi', 'hello', 'hey']) do |left, right|
+  left.length - right.length
 end
