01d5f2b8f0f5a0c1e27e38aae29f8352bee41896
Pacifique LINJANJA <pacilinja1@icloud.com>
2020-12-13 17:34:23 +0100

    Update bubble_sort_by.rb

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,3 +1,26 @@
+arr = [6, 5, 3, 1, 8, 7, 2, 4]
+
+def bubble_sort(arr)
+	sorted = false
+	until sorted == true do
+		exchanged = false
+		arr.each_index do |i|
+			break if i == arr.length-1;
+			curr_value = arr[i]
+			next_value = arr[i + 1]
+			if curr_value > next_value
+				arr[i] = next_value
+				arr[i + 1] = curr_value
+				exchanged = true
+			end
+		end
+		sorted = !exchanged ? true : false;
+		p arr
+	end
+	# p arr
+	arr
+end
+
 def bubble_sort_by(arr)
   puts arr if arr.length <= 1
   n = arr.length - 1
